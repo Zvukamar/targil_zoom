79725d815c5e9678331eaa29c841c354db992fb4
Alex Havkin <fre3run@gmail.com>
2021-03-18 20:51:25 +0200

    clean initial code

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
