d49a65e209106cf1d54a7e554cf0d0279e3fea9b
Alex Havkin <fre3run@gmail.com>
2021-03-19 21:07:57 +0200

    added real data to backend

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,7 +1,20 @@
+const products = require('../data.json');
+
+// By Default return 10 objects from the json array
 const get_all_products = (req, res) => {
-    res.send("Products");
-}
+    const { from, count = 10 } = req.query;
+    if (from === undefined || from === null) {
+        return res.json(products);
+    } else {
+        const fromIndex = parseInt(from);
+        const howMany = parseInt(count);
+
+        const _data = products.slice(fromIndex, fromIndex + howMany);
+        return res.json(_data);
+    }
+};
+
 
 module.exports = {
-    get_all_products
-}
+    get_all_products,
+};
